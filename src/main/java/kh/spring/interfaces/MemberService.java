ea424822a28c9879a932802e5ff183148a239abc
Unknown <plmn855000@gmail.com>
2018-08-14 01:56:01 +0900

    수정-지은

--- src/main/java/kh/spring/interfaces/MemberService.java
+++ src/main/java/kh/spring/interfaces/MemberService.java
@@ -9,6 +9,7 @@ import kh.spring.dto.MemberDTO;
 import kh.spring.dto.ProfileHomePicDTO;
 import kh.spring.dto.ReservationDTO;
 import kh.spring.dto.ReviewDTO;
+import kh.spring.dto.Review_H_DTO;
 
 public interface MemberService {
 
@@ -39,4 +40,8 @@ public interface MemberService {
 	public int updateReservation(int home_seq,String member_email);
 	
 	public List<ReviewDTO> getGuestReview(String member_email);
+	
+	public List<Integer> getSeq(String userId);
+	
+	public List<Review_H_DTO> getHostReview(List<Integer> home_seq);
 }
